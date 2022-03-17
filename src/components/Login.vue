4f882df88546f615b1777d0b758a6e21524fa71a
Gilles Bodart <gilles.bodart@engie.com>
2022-03-17 22:27:26 +0100

    attempt to render web page on heroku without building

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+<template>
+  <form>
+    <div>
+      <label name="username">Username</label>
+      <input type="text" v-model="username">
+    </div>
+    <div>
+      <label name="password">Password</label>
+      <input type="password" v-model="password">
+    </div>
+  </form>
+
+  <h1>{{ username }}</h1>
+  <h1>{{ password }}</h1>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
